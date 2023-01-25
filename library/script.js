aea6a4176da3b427b6346ed8b0ae9fa452d6f86a
Paul <paulnguyen0417@gmail.com>
2023-01-24 22:47:22 -0500

    Basic HTML + CSS & Pop Up to Add Books

---
+++ library/script.js
@@ -0,0 +1,27 @@
+// Add new item popup function
+const openPopUpButton = document.querySelector('[data-popup-open]');
+const closePopUpButton = document.querySelector('[data-popup-close]');
+const overlayPopUp = document.querySelector('.overlay');
+const popUp = document.querySelector('.add-book-popup');
+
+openPopUpButton.addEventListener('click', () => {
+    openPopUp();
+});
+
+closePopUpButton.addEventListener('click', () => {
+    closePopUp();
+});
+
+overlayPopUp.addEventListener('click', () => {
+    closePopUp();
+});
+
+const openPopUp = function() {
+    popUp.setAttribute('id', 'active');
+    overlayPopUp.setAttribute('id', 'active');
+}
+
+const closePopUp = function() {
+    popUp.removeAttribute('id');
+    overlayPopUp.removeAttribute('id');
+}

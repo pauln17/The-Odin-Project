5cc4ddefbd089dd12c2afaae640dd2a4740b4e0a
Paul <paulnguyen0417@gmail.com>
2023-01-26 14:24:53 -0500

    Implemented JS Library Array & Book Constructor

--- library/script.js
+++ library/script.js
@@ -27,3 +27,33 @@ const closePopUp = function() {
     overlayPopUp.removeAttribute('id');
     openPopUpButton.setAttribute('id', 'active');
 }
+
+
+// Obtains user-input and creates a book object from book constructor and then stores it into library array
+let myLibrary = [];
+
+function Book(title, author, pages, pagesRead) {
+    this.title = title;
+    this.author = author;
+    this.pages= pages;
+    this.pagesRead = pagesRead;
+}
+
+function addBookToLibrary(event) {
+    const title = document.querySelector('#title');
+    const author = document.querySelector('#author');
+    const pages = document.querySelector('#total-pages');
+    const pagesRead = document.querySelector('#pages-read');
+
+    const newBook = new Book(title.value, author.value, pages.value, pagesRead.value);
+    myLibrary.push(newBook);
+}
+
+// Prevents default form submission
+const form = document.getElementById('book-form');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    addBookToLibrary()
+})
+
+

0c80f9f0ac75ab95b00523ae03bde260116d7522
Paul <paulnguyen0417@gmail.com>
2023-02-11 22:21:54 -0500

    Script Changes

--- restaurant-page/src/index.js
+++ restaurant-page/src/index.js
@@ -1,5 +1,50 @@
-import createHome from './modules/home';
+import createHomeInfo from './modules/home';
 import './styles.css';
 import steak from './images/steak.jpg';
 
-createHome();
+function createNavBar() {
+    // NavBar Elements
+    const navBar = document.createElement('div');
+    navBar.classList.add('navBar');
+
+    const navHeader = document.createElement('h1');
+    navHeader.innerHTML = 'STEAKHOUSE';
+
+    const navContainer = document.createElement('div');
+    navContainer.classList.add('navContainer');
+
+    // Unordered List and Header Links
+    const navTabs = document.createElement('ul');
+    navTabs.classList.add('navTabs');
+
+    const navLi = document.createElement('li');
+    const navLink = document.createElement('a');
+    navLink.innerHTML = 'Home';
+    navLi.append(navLink);
+
+    const navLi2 = document.createElement('li');
+    const navLink2 = document.createElement('a');
+    navLink2.innerHTML = 'Menu';
+    navLi2.append(navLink2);
+
+    const navLi3 = document.createElement('li');
+    const navLink3 = document.createElement('a');
+    navLink3.innerHTML = 'About';
+    navLi3.append(navLink3);
+
+    // Appending Items to Content
+    navTabs.append(navLi, navLi2, navLi3);
+    navContainer.append(navHeader, navTabs);
+    navBar.append(navContainer);
+    
+    return navBar
+}
+
+function initializeWebsite() {
+    const content = document.querySelector('#content');
+
+    content.append(createNavBar());
+    // content.append(createHomeInfo());
+}
+
+initializeWebsite();
